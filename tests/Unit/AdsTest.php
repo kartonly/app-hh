6f9c39a2af29a3beeb21ccb2e0afc035d8075cf1
kartonly <kartonly@yandex.ru>
2022-07-14 22:01:42 +0300

    add test

--- tests/Unit/AdsTest.php
+++ tests/Unit/AdsTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Faker\Generator as Faker;
 
 class AdsTest extends TestCase
 {
@@ -11,8 +13,30 @@ class AdsTest extends TestCase
      *
      * @return void
      */
-    public function testSomethingIsTrue()
+
+    use WithFaker;
+
+
+    public function testOne()
     {
+        $this->call('GET', 'one/1');
+
+        $this->assertTrue(true);
+    }
+
+    public function testCreate()
+    {
+        $array = [];
+        for($i = 0; $i <= 2; $i++){
+            array_push($array, $this->faker->url());
+        }
+        $this->call('POST', 'create', array(
+            'name' => $this->faker->name(),
+            'about' => $this->faker->text(),
+            'price' => $this->faker->randomNumber(),
+            'link'=> $array
+        ));
+
         $this->assertTrue(true);
     }
 }
